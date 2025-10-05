37ec047ec42d6f30f2e05b2b0e1f0de7b1d42ec7
Adriana <adriaguadog@gmail.com>
2025-10-05 10:48:15 +0200

    update

--- proyectoinicial/src/MainCreated.java
+++ proyectoinicial/src/MainCreated.java
@@ -1,2 +1,14 @@
 public class MainCreated {
+
+    public static void main(String[] args){
+
+        System.out.println("este es mi primer programa en java");
+        System.out.println("este es mi segundo programa en java");
+        System.out.print("este es mi primer programa en java");
+        System.out.print("este es mi segundo programa en java");
+// si pongo ln tras el print, me hace salto de linea.
+// public quiere decir que to.do el mundo lo puede ver, static porque es obligatorio y no se mueve, void porque no genera un resultado.
+// TODO para cosas pendientes de terminar. Si luego ledoy a view-tool-TODO me sale una ventana con todo lo pendiente
+//cuando le doy a run, se crea una carpeta out con un archivo.class del mismo nombre, que es lo que recibe la JVM para leer.
+    }
 }
